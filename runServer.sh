25a297c28e0ed91d462f06f5cd6fffb90a023276
huisongyang <yanghuisong@foxmail.com>
2016-08-10 16:14:58 +0800

    update file

--- runServer.sh
+++ runServer.sh
@@ -7,4 +7,4 @@ root_dir_exclude=
 root_dir_excludeDir=
 
 #Ä¬ÈÏ¶Ë¿ÚÊÇ9999
-java -Droot.dir.path=${root_dir_path} -Droot.dir.exclude=${root_dir_exclude} -Droot.dir.excludeDir=${root_dir_excludeDir} -cp autoupdate-2.0.0-SNAPSHOT.jar com.yhs.autoupdate.server.Server
+java -Droot.dir.path=${root_dir_path} -Droot.dir.exclude=${root_dir_exclude} -Droot.dir.excludeDir=${root_dir_excludeDir} -cp fileserver-0.0.1-SNAPSHOT.jar com.yhs.fileserver.server.Server
